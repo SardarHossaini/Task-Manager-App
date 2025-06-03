8d64b9b77d945d442c040ecc378df4ca632702eb
Sardar219 <sardarhossaini2022@gmail.com>
2025-06-03 15:30:06 +0430

    add the taskRepository class

--- lib/app/data/providers/task/provider.dart
+++ lib/app/data/providers/task/provider.dart
@@ -6,7 +6,14 @@ import 'package:task_manager_app/app/data/models/task.dart';
 import 'package:task_manager_app/app/data/services/storage/services.dart';
 
 class TaskProvider {
-  StorageService _storage = Get.find<StorageService>();
+  final _storage = Get.find<StorageService>();
+
+  // {'tasks':[
+  //   {'title':'Work',
+  //   'color':'#ff123456',
+  //   'icon':0xe123
+  //   }
+  // ]}
 
   List<Task> readTasks() {
     var tasks = <Task>[];
@@ -14,5 +21,9 @@ class TaskProvider {
       _storage.read(taskKey).toString(),
     ).forEach((e) => tasks.add(Task.fromJson(e)));
     return tasks;
+  }
+
+  void writeTasks(List<Task> tasks) {
+    _storage.write(taskKey, jsonEncode(tasks));
   }
 }
