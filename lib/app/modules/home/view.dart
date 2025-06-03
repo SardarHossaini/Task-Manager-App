2d3e88906bfc3a40572fa528697fd1cca15ee75b
Sardar219 <sardarhossaini2022@gmail.com>
2025-06-03 10:42:26 +0430

    add the getx to start

---
+++ lib/app/modules/home/view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to another page or perform an action
+              },
+              child: const Text('Go to Next Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
