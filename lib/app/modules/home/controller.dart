bba6a9fde4fc74f7cb3082d4d8f68badee7dbf8f
Sardar Hossaini <sardarhossaini2022@gmail.com>
2025-08-06 13:49:06 +0430

    implement task update functionality and enhance add dialog with validation

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -11,6 +11,7 @@ class HomeController extends GetxController {
   final chipIndex = 0.obs;
   final deleting = false.obs;
   final tasks = <Task>[].obs;
+  final task = Rx<Task?>(null);
 
   @override
   void onInit() {
@@ -35,6 +36,10 @@ class HomeController extends GetxController {
     deleting.value = value;
   }
 
+  void changeTask(Task? task) {
+    this.task.value = task;
+  }
+
   bool addTask(Task task) {
     if (tasks.contains(task)) {
       return false;
@@ -46,5 +51,23 @@ class HomeController extends GetxController {
 
   void deleteTask(Task task) {
     tasks.remove(task);
+  }
+
+  updateTask(Task task, String title) {
+    var todos = task.todos ?? [];
+    if (containeTodo(todos, title)) {
+      return false;
+    }
+    var todo = {'title': title, 'done': false};
+    todos.add(todo);
+    var newTask = task.copyWith(todos: todos);
+    int oldIdx = tasks.indexOf(task);
+    tasks[oldIdx] = newTask;
+    tasks.refresh();
+    return true;
+  }
+
+  bool containeTodo(List todos, String title) {
+    return todos.any((todo) => todo['title'] == title);
   }
 }
--- lib/app/modules/home/widgets/add_dialog.dart
+++ lib/app/modules/home/widgets/add_dialog.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:task_manager_app/app/core/utils/extensions.dart';
+import 'package:task_manager_app/app/data/models/task.dart';
 import 'package:task_manager_app/app/modules/home/controller.dart';
 
 class AddDialog extends StatelessWidget {
@@ -22,6 +24,8 @@ class AddDialog extends StatelessWidget {
                 IconButton(
                   onPressed: () {
                     Get.back();
+                    homeCtrl.editController.clear();
+                    homeCtrl.changeTask(null);
                   },
                   icon: const Icon(Icons.close),
                 ),
@@ -29,7 +33,26 @@ class AddDialog extends StatelessWidget {
                   style: TextButton.styleFrom(
                     backgroundColor: Colors.transparent,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    if (homeCtrl.formKey.currentState!.validate()) {
+                      if (homeCtrl.task.value == null) {
+                        EasyLoading.showError("Please select a task to add");
+                      } else {
+                        var success = homeCtrl.updateTask(
+                          homeCtrl.task.value!,
+                          homeCtrl.editController.text,
+                        );
+                        if (success) {
+                          EasyLoading.showSuccess("Task updated successfully");
+                          Get.back();
+                          homeCtrl.changeTask(null);
+                        } else {
+                          EasyLoading.showError("Task already exists");
+                        }
+                        homeCtrl.editController.clear();
+                      }
+                    }
+                  },
                   child: Text(
                     "Done",
                     style: TextStyle(fontSize: 14.0.sp, color: Colors.blue),
@@ -45,19 +68,76 @@ class AddDialog extends StatelessWidget {
               style: TextStyle(fontSize: 20.0.sp, fontWeight: FontWeight.bold),
             ),
           ),
-          TextFormField(
-            controller: homeCtrl.editController,
-            decoration: InputDecoration(
-              labelText: "Title",
-              border: OutlineInputBorder(),
+          Padding(
+            // padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+            padding: EdgeInsets.all(5.0.wp),
+            child: TextFormField(
+              controller: homeCtrl.editController,
+              decoration: InputDecoration(
+                labelText: "Title",
+                border: OutlineInputBorder(),
+              ),
+              autofocus: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a task name";
+                }
+                return null;
+              },
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please enter a task name";
-              }
-              return null;
-            },
           ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 5.0.wp,
+              right: 5.0.wp,
+              bottom: 2.0.wp,
+            ),
+            child: Text(
+              "Add to",
+              style: TextStyle(fontSize: 14.0.sp, color: Colors.grey),
+            ),
+          ),
+          ...homeCtrl.tasks
+              .map(
+                (element) => Obx(
+                  () => InkWell(
+                    onTap: () => homeCtrl.changeTask(element),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 3.0.wp,
+                        horizontal: 5.0.wp,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                IconData(
+                                  element.icon,
+                                  fontFamily: 'MaterialIcons',
+                                ),
+                                color: HexColor.fromHex(element.color),
+                              ),
+                              SizedBox(width: 3.0.wp),
+                              Text(
+                                element.title,
+                                style: TextStyle(
+                                  fontSize: 12.0.sp,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          if (homeCtrl.task.value == element)
+                            const Icon(Icons.check, color: Colors.blue),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+              .toList(),
         ],
       ),
     );
